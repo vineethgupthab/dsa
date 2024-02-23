db80c14370db822191a6536f0677b1a4d2ae5085
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-02-23 13:59:18 -0800

    Time: 461 ms (17.36%) | Memory: 28.2 MB (99.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,6 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums_freq = dict(); max = 0
+        '''nums_freq = dict(); max = 0
         for i in nums:
             if i in nums_freq:
                 nums_freq[i]+=1
@@ -10,4 +10,13 @@ class Solution:
         if max>=2:
             return True
         else:
-            return False
+            return False'''
+        nums.sort()
+        i=0
+        j=i+1
+        while j<len(nums):
+            if nums[i]==nums[j]:
+                return True
+            i+=1
+            j+=1
+        return False
