276c8dbd56e411e20151aed25b5881ada1832758
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-02-23 13:55:06 -0800

    Time: 467 ms (15.09%) | Memory: 35 MB (11.79%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums_freq = dict()
+        nums_freq = dict(); max = 0
         for i in nums:
             if i in nums_freq:
                 nums_freq[i]+=1
+                max = nums_freq[i]
             else:
                 nums_freq[i] = 1
-        if sorted(nums_freq.values(), reverse=True)[0]>=2:
+        if max>=2:
             return True
         else:
             return False
