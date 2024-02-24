6e20fd4dda05dbdbabdbdb6d52fb4907594aa6b8
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-02-24 02:05:33 -0800

    Time: 44 ms (82.20%) | Memory: 16.7 MB (94.07%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,11 +1,10 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans=[]
-        dc=defaultdict(lambda:0)
-        for a in nums1:
-            dc[a]=1
-        nums2=set(nums2)
-        for a in nums2:
-            if(dc[a]==1):
-                ans.append(a)
-        return ans
+        result = set()
+        freq = defaultdict(lambda:0)
+        for num in nums1:
+            freq[num]=1
+        for num in nums2:
+            if freq[num]==1:
+                result.add(num)
+        return result
