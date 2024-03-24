dc6a72931a249d9954b6be8395339a6abf6fffbe
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-03-24 15:48:30 -0700

    Time: 491 ms (69.61%) | Memory: 29.7 MB (19.15%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,20 +1,11 @@
 class Solution:
-    def maxOperations(self,nums, k):
-        left = 0; right = len(nums)-1; count = 0
-        nums.sort()
-        while left<right:
-            if nums[left]+nums[right]==k:
-                count+=1
-                #print(left, right, nums)
-                nums.pop(left)
-                left=left
-                right=right-1
-                nums.pop(right)
-                right=right-1
-                #print(left, right, nums)
-                continue
-            elif nums[left]+nums[right]<k:
-                left+=1
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        hash_map = dict()
+        num_of_operation = 0
+        for i in nums:
+            if i in hash_map and hash_map[i] > 0:
+                hash_map[i] -= 1
+                num_of_operation += 1
             else:
-                right-=1
-        return count
+                hash_map[k-i] = hash_map.get(k-i, 0) + 1
+        return num_of_operation
