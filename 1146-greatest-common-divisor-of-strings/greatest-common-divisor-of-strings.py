7f8784d492806850b2a8283acae974531fce0f8b
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-04-04 15:24:59 -0700

    Time: 34 ms (73.85%) | Memory: 16.6 MB (68.70%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,12 +1,17 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        common = ''
-        min_length = min(len(str1), len(str2))
-        
-        for i in range(1, min_length + 1):
-            if len(str1) % i == 0 and len(str2) % i == 0:
-                substr = str1[:i]
-                if substr * (len(str1) // i) == str1 and substr * (len(str2) // i) == str2:
-                    common = substr
-                    
-        return common
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        def is_divisor(divisor, string):
+            return all(string[i:i+len(divisor)] == divisor for i in range(0, len(string), len(divisor)))
+
+        gcd_len = gcd(len(str1), len(str2))
+        gcd_str = str1[:gcd_len]
+
+        if is_divisor(gcd_str, str1) and is_divisor(gcd_str, str2):
+            return gcd_str
+        else:
+            return ""
