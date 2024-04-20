33740e60c1453079355aee5d42a2e6e4808f4693
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-04-20 00:53:28 -0700

    Time: 514 ms (24.28%) | Memory: 30.5 MB (7.58%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,33 +1,14 @@
-'''class Solution:
-    def maxOperations(self,nums, k):
-        left = 0; right = len(nums)-1; count = 0
+class Solution:
+    def maxOperations(self, nums, k):
         nums.sort()
+        left = 0; right = len(nums)-1; matches = dict()
         while left<right:
             if nums[left]+nums[right]==k:
-                count+=1
-                nums.pop(left)
-                left=left
-                right=right-1
-                nums.pop(right)
-                right=right-1
-                continue
+                matches[left]=right
+                left+=1
+                right-=1
             elif nums[left]+nums[right]<k:
                 left+=1
             else:
                 right-=1
-        return count'''
-
-class Solution:
-    def maxOperations(self, nums, k):
-        count = 0
-        num_freq = {}
-        
-        for num in nums:
-            complement = k - num
-            if complement in num_freq and num_freq[complement] > 0:
-                count += 1
-                num_freq[complement] -= 1
-            else:
-                num_freq[num] = num_freq.get(num, 0) + 1
-        
-        return count
+        return len(matches.keys())
