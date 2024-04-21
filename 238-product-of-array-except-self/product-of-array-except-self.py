31c83146e0b049a1ca22b92855ddaa5d2db2203c
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-04-21 13:06:06 -0700

    Time: 268 ms (23.24%) | Memory: 26.3 MB (9.57%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -8,5 +8,7 @@ class Solution:
         for i in range(len(nums)-2, -1,-1):
             rtol_products[i] = nums[i+1]*rtol_products[i+1]
         
-        return [ltor_products[i]*rtol_products[i] for i in range(len(ltor_products))]
+        result = [ltor_products[i]*rtol_products[i] for i in range(len(ltor_products))]
+        return result
+
 
